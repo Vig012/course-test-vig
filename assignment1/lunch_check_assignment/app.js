2a736dc5fe7e8b065c5ecd9215f4d459ef27c0b9
Vig012 <vigneshdevadiga012@gmail.com>
2023-07-16 23:52:25 +0530

    Assignment-1 is Complete

---
+++ assignment1/lunch_check_assignment/app.js
@@ -0,0 +1,27 @@
+(function () {
+
+angular.module('LunchCheckApp', [])
+
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject=['$scope'];
+
+function LunchCheckController($scope) {
+  $scope.stringItem='';
+  $scope.message="";
+
+  $scope.checkLunch = function () {
+    var itemCount = $scope.stringItem.split(',');
+
+    if($scope.stringItem.trim() === ''){
+      $scope.message = 'Please enter data first';
+    }
+    else if(itemCount.length <=3){
+      $scope.message = 'Enjoy!';
+    }
+    else{
+      $scope.message = 'Too much!';
+    }
+  };
+}
+})();
